c6116ece4b9eaf8f8d32e7acd2ee06b8ea363a43
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-10-26 15:34:04 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
 
 /* List the name of all animals that are neutered and have less than 3 escape attempts. */
-SELECT name FROM animals WHERE neutered = true AND escape_attempts >= 3;
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 
 /* List the date of birth of all animals named either "Agumon" or "Pikachu". */
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
