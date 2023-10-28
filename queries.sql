cb2db3d61c8f8a536e001f74e4bfea631b1a96a1
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-10-28 22:18:40 +0100

    Update queries

--- queries.sql
+++ queries.sql
@@ -23,3 +23,87 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 /* Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/* Update the species column to "unspecified" and then roll back the change: */
+-- Start a transaction
+BEGIN;
+
+-- Update the species to "unspecified" for all animals
+UPDATE animals
+SET species = 'unspecified';
+
+-- Verify the change
+SELECT * FROM animals;
+
+-- Roll back the transaction
+ROLLBACK;
+
+-- Verify that species is back to the state before the transaction
+SELECT * FROM animals;
+
+
+/* Update the species column based on specific conditions and commit the transaction: */
+-- Start a new transaction
+BEGIN;
+
+-- Update the species to "digimon" for animals with names ending in "mon"
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+-- Update the species to "pokemon" for animals without a species already set
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+-- Verify the changes
+SELECT * FROM animals;
+
+-- Commit the transaction to save the changes
+COMMIT;
+
+-- Verify that the changes persist after the commit
+SELECT * FROM animals;
+
+/* delete all records in the "animals" table within a transaction and then roll back the transaction */
+-- Start a new transaction
+BEGIN;
+
+-- Delete all records in the "animals" table
+DELETE FROM animals;
+
+-- Verify that records have been deleted (will show an empty table)
+SELECT * FROM animals;
+
+-- Roll back the transaction to undo the deletion
+ROLLBACK;
+
+-- Verify that the records are back to their previous state
+SELECT * FROM animals;
+
+
+/* creation of a savepoint, rolling back to that savepoint */
+-- Start a new transaction
+BEGIN;
+
+-- Delete all animals born after Jan 1st, 2022
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint
+SAVEPOINT my_savepoint;
+
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+-- Roll back to the savepoint
+ROLLBACK TO my_savepoint;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+-- Commit the transaction
+COMMIT;
